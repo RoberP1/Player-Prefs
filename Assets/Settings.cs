79c5c71f65f099ed8ae31c094533387dff81d2eb
Robertino Papeschi <roberpapeschi@gmail.com>
2022-10-18 19:41:07 -0300

    Update Settings.cs

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -8,42 +8,21 @@ public class Settings : MonoBehaviour
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private TMP_Dropdown resolution;
 
-    private void Start()
-    {
-        SetDefault();
-    }
-
+    private void Start() => SetDefault();
+    
     private void SetDefault()
     {
         nameInput.text = PlayerPrefs.HasKey("Name") ?  PlayerPrefs.GetString("Name") : "";
         volumeSlider.value = PlayerPrefs.HasKey("Volume") ? PlayerPrefs.GetFloat("Volume") : 1;
         resolution.value = PlayerPrefs.HasKey("Resolution") ? PlayerPrefs.GetInt("Resolution") : 0;
     }
-    
-
-    public void SetVolumen()
-    {
-        // Guardar los datos del volumen.
-        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
-    }
-
-    public void SetName()
-    {
-        // Guardar los datos del nombre.
-        PlayerPrefs.SetString("Name", nameInput.text);
-    }
-
-    public void SetResolution()
-    {
-        // Guardar los datos de la resolucion.
-        PlayerPrefs.SetInt("Resolution", resolution.value);
-
-    }
-
     public void ResetDefault()
     {
-        // Borrar todos los datos existentes.
         PlayerPrefs.DeleteAll();
         SetDefault();
     }
+    
+    public void SetVolumen() => PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+    public void SetName() => PlayerPrefs.SetString("Name", nameInput.text);
+    public void SetResolution() =>  PlayerPrefs.SetInt("Resolution", resolution.value);
 }
