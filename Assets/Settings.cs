b0f14b422c1e1a40227c9c8d24e2a13a1c48e291
Robertino Papeschi <roberpapeschi@gmail.com>
2022-10-18 19:34:13 -0300

    Save Settings

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -10,26 +10,40 @@ public class Settings : MonoBehaviour
 
     private void Start()
     {
-       
+        SetDefault();
     }
+
+    private void SetDefault()
+    {
+        nameInput.text = PlayerPrefs.HasKey("Name") ?  PlayerPrefs.GetString("Name") : "";
+        volumeSlider.value = PlayerPrefs.HasKey("Volume") ? PlayerPrefs.GetFloat("Volume") : 1;
+        resolution.value = PlayerPrefs.HasKey("Resolution") ? PlayerPrefs.GetInt("Resolution") : 0;
+    }
+    
 
     public void SetVolumen()
     {
         // Guardar los datos del volumen.
+        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
     }
 
     public void SetName()
     {
         // Guardar los datos del nombre.
+        PlayerPrefs.SetString("Name", nameInput.text);
     }
 
     public void SetResolution()
     {
         // Guardar los datos de la resolucion.
+        PlayerPrefs.SetInt("Resolution", resolution.value);
+
     }
 
     public void ResetDefault()
     {
         // Borrar todos los datos existentes.
+        PlayerPrefs.DeleteAll();
+        SetDefault();
     }
 }
